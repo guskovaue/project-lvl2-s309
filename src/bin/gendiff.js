259ef41f9b61a091aa66d9b792fe48e6881336c1
ue.shkolnikova <shkolnikova_yuli@mail.ru>
2018-08-06 17:10:37 +0200

    change package version

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,7 +2,7 @@
 import program from 'commander';
 
 program
-  .version('1.0.0')
+  .version('1.0.1')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'Output format')
