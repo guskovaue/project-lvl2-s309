459491c98e3043718a2faa78a0a242009895d564
ue.shkolnikova <shkolnikova_yuli@mail.ru>
2018-08-06 23:41:59 +0200

    change file extencion

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,7 +3,8 @@ import genDiff from '../src';
 
 test('testDiff', () => {
   const pathToTestFiles = './__tests__/__fixtures__';
-  const currentAnswer = genDiff(`${pathToTestFiles}/test_file1`, `${pathToTestFiles}/test_file2`);
+  const currentAnswer = genDiff(`${pathToTestFiles}/test_file1.json`,
+    `${pathToTestFiles}/test_file2.json`);
   const rightAnswer = fs.readFileSync(`${pathToTestFiles}/answer`).toString();
   expect(currentAnswer).toBe(rightAnswer);
 });
