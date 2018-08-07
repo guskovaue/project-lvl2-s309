21adb8ee2a5859ac0d3699cea1aff4e93bd830df
ue.shkolnikova <shkolnikova_yuli@mail.ru>
2018-08-07 23:43:03 +0200

    reduce duplication code

--- src/parsers/yaml_parser.js
+++ src/parsers/yaml_parser.js
@@ -1,11 +1,8 @@
 import yaml from 'js-yaml';
 import fs from 'fs';
+import JsonParser from './json_parser';
 
-export default class {
-  constructor(filename) {
-    this.filename = filename;
-  }
-
+export default class extends JsonParser{
   parse() {
     return yaml.safeLoad(fs.readFileSync(this.filename));
   }
