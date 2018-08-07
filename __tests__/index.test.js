589d3d549fefd4f31fed846d3fbb95bb70d699ac
ue.shkolnikova <shkolnikova_yuli@mail.ru>
2018-08-07 23:22:14 +0200

    add test for yaml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,18 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-test('testDiff', () => {
+test('testDiffJson', () => {
   const pathToTestFiles = './__tests__/__fixtures__';
   const currentAnswer = genDiff(`${pathToTestFiles}/test_file1.json`,
     `${pathToTestFiles}/test_file2.json`);
+  const rightAnswer = fs.readFileSync(`${pathToTestFiles}/answer`).toString();
+  expect(currentAnswer).toBe(rightAnswer);
+});
+
+test('testDiffYaml', () => {
+  const pathToTestFiles = './__tests__/__fixtures__';
+  const currentAnswer = genDiff(`${pathToTestFiles}/test_file3.yaml`,
+    `${pathToTestFiles}/test_file4.yaml`);
   const rightAnswer = fs.readFileSync(`${pathToTestFiles}/answer`).toString();
   expect(currentAnswer).toBe(rightAnswer);
 });
