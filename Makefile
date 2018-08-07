243231ecf9d6433249ec242a97f13ffbeaa6a99b
ue.shkolnikova <shkolnikova_yuli@mail.ru>
2018-08-07 19:35:57 +0200

    new version of genDiff with union and map

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ install:
 	npm install
 
 start:
-	npm run babel-node -- src/flow.js /Users/homyak/Documents/repos/hexlet_projects/file1 /Users/homyak/Documents/repos/hexlet_projects/file2
+	npm run babel-node -- src/flow.js /Users/homyak/Documents/repos/hexlet_projects/file1.json /Users/homyak/Documents/repos/hexlet_projects/file2.json
 
 publish:
 	npm publish
--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
 import fs from 'fs';
-import { has } from 'lodash';
+import yaml from 'js-yaml';
+import { has, union } from 'lodash';
 
 const genDiff = (firstFile, secondFile) => {
-  const cfgData1 = JSON.parse(fs.readFileSync(`${firstFile}`));
-  const cfgData2 = JSON.parse(fs.readFileSync(`${secondFile}`));
+  const cfgData1 = yaml.safeLoad(fs.readFileSync(`${firstFile}`));
+  const cfgData2 = yaml.safeLoad(fs.readFileSync(`${secondFile}`));
 
-  const diffFirstRelativelySecond = Object.keys(cfgData1).reduce((acc, key) => {
+  const commonKeys = union(Object.keys(cfgData1), Object.keys(cfgData2));
+
+  const fullDiff = commonKeys.map((key) => {
     const cfgDataValue1 = cfgData1[key];
-    if (has(cfgData2, `${key}`)) {
-      const cfgDataValue2 = cfgData2[key];
+    const cfgDataValue2 = cfgData2[key];
+    if (has(cfgData1, key) && has(cfgData2, key)) {
       if (cfgDataValue1 === cfgDataValue2) {
-        return [...acc, `    ${key}: ${cfgDataValue1}`];
+        return `    ${key}: ${cfgDataValue1}`;
       }
-      return [...acc, `  - ${key}: ${cfgDataValue1}`, `  + ${key}: ${cfgDataValue2}`];
+      return `  - ${key}: ${cfgDataValue1}\n  + ${key}: ${cfgDataValue2}`;
     }
-    return [...acc, `  - ${key}: ${cfgDataValue1}`];
-  }, []);
 
-  const fullDiff = Object.keys(cfgData2).reduce((acc, key) => {
-    if (!has(cfgData1, `${key}`)) {
-      return [...acc, `  + ${key}: ${cfgData2[key]}`];
+    if (has(cfgData1, key)) {
+      return `  - ${key}: ${cfgDataValue1}`;
     }
-    return acc;
-  }, diffFirstRelativelySecond);
-
+    return `  + ${key}: ${cfgDataValue2}`;
+  });
   return ['{', ...fullDiff, '}'].join('\n');
 };
 
