cc5ddae01ab44c03f86bf2c9bf5f7e426876aa29
ue.shkolnikova <shkolnikova_yuli@mail.ru>
2018-08-11 20:51:30 +0200

    add test for plain format

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,14 +3,23 @@ import genDiff from '../src';
 
 const pathToTestFiles = './__tests__/__fixtures__';
 
-const check = (file1, file2) => {
-  const currentAnswer = genDiff(`${pathToTestFiles}/${file1}`, `${pathToTestFiles}/${file2}`);
-  const rightAnswer = fs.readFileSync(`${pathToTestFiles}/answer`).toString();
+
+const check = (file1, file2, answerFileName, type) => {
+  const currentAnswer = genDiff(`${pathToTestFiles}/${file1}`, `${pathToTestFiles}/${file2}`, type);
+  const rightAnswer = fs.readFileSync(`${pathToTestFiles}/${answerFileName}`).toString();
   return expect(currentAnswer).toBe(rightAnswer);
 };
 
-test('testDiffJson', () => check('test_file1.json', 'test_file2.json'));
+// tests for common rendering
+test('testDiffJson', () => check('test_file1.json', 'test_file2.json', 'answer'));
 
-test('testDiffYaml', () => check('test_file3.yaml', 'test_file4.yaml'));
+test('testDiffYaml', () => check('test_file3.yaml', 'test_file4.yaml', 'answer'));
 
-test('testDiffIni', () => check('test_file5.ini', 'test_file6.ini'));
+test('testDiffIni', () => check('test_file5.ini', 'test_file6.ini', 'answer'));
+
+// tests for plain rendering
+test('testPlainDiffJson', () => check('test_file1.json', 'test_file2.json', 'plain_answer', 'plain'));
+
+test('testPlainDiffYaml', () => check('test_file3.yaml', 'test_file4.yaml', 'plain_answer', 'plain'));
+
+test('testPlainDiffIni', () => check('test_file5.ini', 'test_file6.ini', 'plain_answer', 'plain'));
