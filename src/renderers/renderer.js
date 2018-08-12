9532474f75308cacdbc64f30cd08edae91eb437c
ue.shkolnikova <shkolnikova_yuli@mail.ru>
2018-08-12 21:06:08 +0200

    simplify render function

--- src/renderers/renderer.js
+++ src/renderers/renderer.js
@@ -3,20 +3,14 @@ const renderObject = (object, offset = 0) => {
     return object;
   }
   const padding = '  '.repeat(offset);
-  return [
-    '{',
-    ...Object.keys(object).map((key) => {
-      const value = object[key];
-      if (value instanceof Object) {
-        return [
-          `  ${padding}${key}: `,
-          ...renderObject(value, offset + 1),
-        ].join('');
-      }
-      return `    ${padding}${key}: ${value}`;
-    }),
-    `${padding}}`,
-  ].join('\n');
+  const res = Object.keys(object).map((key) => {
+    const value = object[key];
+    if (value instanceof Object) {
+      return `  ${padding}${key}: ${renderObject(value, offset + 1)}`;
+    }
+    return `    ${padding}${key}: ${value}`;
+  }).join('\n');
+  return `{\n${res}\n${padding}}`;
 };
 
 const processRenderer = (node, offset = 1) => {
@@ -47,7 +41,7 @@ const processRenderer = (node, offset = 1) => {
 
 const render = (ast) => {
   const res = ast.map(node => processRenderer(node)).join('\n');
-  return `{\n${res}\n}\n`;
+  return `{\n${res}\n}`;
 };
 
 export default render;
